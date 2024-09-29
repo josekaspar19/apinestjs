130e61eaefaaa7968751871b0c5a8894436c06e0
Jose Kaspar <jkasparm@gmail.com>
2024-09-29 12:49:38 -0400

    First commit to deploy

---
+++ src/tasks/task.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { TaskController } from './task.controller';
+import { TasksService } from './task.service';
+
+@Module({
+    controllers: [TaskController],
+    providers: [TasksService]
+})
+export class TasksModule {}
