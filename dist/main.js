507cc12451580194de4334d4ab4297edcac04ec0
Jose Kaspar <jkasparm@gmail.com>
2024-09-29 13:42:42 -0400

    change env variables

--- dist/main.js
+++ dist/main.js
@@ -17,6 +17,7 @@ async function bootstrap() {
     const document = swagger_1.SwaggerModule.createDocument(app, config);
     swagger_1.SwaggerModule.setup('api', app, document);
     app.enableCors();
+    app.setGlobalPrefix('');
     await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
